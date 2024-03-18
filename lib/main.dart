2bcbdd3513a6ce23b5bc2214441dd44ca5f31838
MFantiniT <matheusfantini@gmail.com>
2024-03-17 21:54:22 -0300

    att

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'router/app_router.dart';
-
+import 'modules/login/login_screen.dart'; 
 void main() {
   runApp(MyApp());
 }
@@ -10,7 +10,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'MyMoney',
+      theme: ThemeData(
+        primarySwatch: Colors.green, 
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        
+      ),
+      home: LoginScreen(), // Define a tela de login como a inicial
       onGenerateRoute: AppRouter.onGenerateRoute,
+      
     );
   }
 }
