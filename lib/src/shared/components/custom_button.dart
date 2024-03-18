72a306888958384071fbf0474d63ff9e942a5fdb
MFantiniT <matheusfantini@gmail.com>
2024-03-17 22:01:04 -0300

    att

--- lib/src/shared/components/custom_button.dart
+++ lib/src/shared/components/custom_button.dart
@@ -3,18 +3,18 @@ import 'package:flutter/material.dart';
 class CustomButton extends StatelessWidget {
   final String text;
   final VoidCallback onPressed;
+  final Color color; // Adicionei a cor como um parâmetro
 
   const CustomButton({
-    Key key,
-    this.text,
-    this.onPressed,
+    Key? key, 
+    required this.text, 
+    required this.onPressed,
+    this.color = Colors.blue, 
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: onPressed,
-      child: Text(text),
-    );
-  }
-}
+      style: ElevatedButton.styleFrom(
+        primary: color,
