0cd833d661132be8f993a05b7a2c530a9de6a126
MFantiniT <matheusfantini@gmail.com>
2024-03-24 13:17:49 -0300

    corrigindo

--- lib/src/modules/login/login_screen.dart
+++ lib/src/modules/login/login_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mymoney/src/shared/components/app_loading.dart';
 import '../../shared/components/custom_button.dart';
-// Certifique-se de importar a página de registro se necessário
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -11,84 +10,84 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  // Controladores para os campos de texto
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
-  bool isLoading = true;
-
-  @override
-  void initState() {
-    super.initState();
-    Future.delayed(
-      const Duration(seconds: 5),
-    ).then((_) => setState(() {
-          isLoading = false;
-        }));
-  }
+  bool isLoading = false;
 
   void _navigateToRegister() {
-    // Use o Navigator para mudar para a tela de registro
-    Navigator.of(context).pushNamed('/register'); // Certifique-se de que esta rota está definida no seu gerenciador de rotas
+    Navigator.of(context).pushNamed('/register');
+  }
+
+  void _login() async {
+    setState(() {
+      isLoading = true; // Ativar o indicador de carregamento
+    });
+
+    // Simular uma chamada de API
+    await Future.delayed(const Duration(seconds: 2));
+
+    // Após a chamada da API ou operação de login
+    setState(() {
+      isLoading = false;
+    });
+
+    // Aqui adicionaremos a lógica de navegação após o login bem sucedido
   }
 
   @override
   Widget build(BuildContext context) {
-    if (isLoading) {
-      return const Center(
-        child: AppLoading(),
-      );
-    } else {
-      return Scaffold(
-        appBar: AppBar(
-          title: const Text('Login'),
-          backgroundColor: Colors.green,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+        backgroundColor: Colors.green,
+      ),
+      body: isLoading
+          ? const Center(
+        child: AppLoading(), // Exibir o loading enquanto isLoading for true
+      )
+          : Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            TextFormField(
+              controller: _emailController,
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                labelText: 'Email',
+                hintText: 'Digite seu e-mail',
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.email),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            TextFormField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(
+                labelText: 'Senha',
+                hintText: 'Digite sua senha',
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.lock),
+              ),
+            ),
+            const SizedBox(height: 24.0),
+            CustomButton(
+              text: 'ENTRAR',
+              onPressed: _login, // Adiciona a lógica de login ao botão
+              color: Colors.greenAccent,
+            ),
+            const SizedBox(height: 16.0),
+            CustomButton(
+              text: 'Registrar-se',
+              onPressed: _navigateToRegister,
+              color: Colors.greenAccent, // Cor do botão igual ao botão ENTRAR
+            ),
+          ],
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              TextFormField(
-                controller: _emailController,
-                keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(
-                  labelText: 'Email',
-                  hintText: 'Digite seu e-mail',
-                  border: OutlineInputBorder(),
-                  prefixIcon: Icon(Icons.email),
-                ),
-              ),
-              const SizedBox(height: 16.0),
-              TextFormField(
-                controller: _passwordController,
-                obscureText: true,
-                decoration: const InputDecoration(
-                  labelText: 'Senha',
-                  hintText: 'Digite sua senha',
-                  border: OutlineInputBorder(),
-                  prefixIcon: Icon(Icons.lock),
-                ),
-              ),
-              const SizedBox(height: 24.0),
-              CustomButton(
-                text: 'ENTRAR',
-                onPressed: () {
-                  // Aqui adicionaremos a lógica de login
-                },
-                color: Colors.greenAccent,
-              ),
-              const SizedBox(height: 16.0),
-              CustomButton(
-                text: 'Registrar-se',
-                onPressed: _navigateToRegister,
-                color: Colors.blueAccent, // Uma cor diferente para o botão de registro
-              ),
-            ],
-          ),
-        ),
-      );
-    }
+      ),
+    );
   }
 }
--- lib/src/router/app_router.dart
+++ lib/src/router/app_router.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
 import '../modules/login/login_screen.dart';
 import '../modules/register/page/register_page.dart';
+import '../modules/register/controller/register_controller.dart';
+import '../modules/register/repositories/register_repository.dart';
+import '../modules/register/service/register_service.dart';
 
 class AppRouter {
   static Route<dynamic> onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
       case '/login':
-        // Rota para a tela de login
         return MaterialPageRoute(builder: (_) => const LoginScreen());
       case '/register':
-        // Rota para a tela de registro
-        return MaterialPageRoute(builder: (_) => RegisterPage()); // Substitua com a instância correta da sua tela de registro
-      
+
+        final registerController = RegisterController(RegisterRepository(RegisterService()));
+        return MaterialPageRoute(builder: (_) => RegisterPage(registerController));
       default:
-        // Se a rota não for definida, informe o usuário
         return MaterialPageRoute(builder: (_) => Scaffold(body: Center(child: Text('No route defined for ${settings.name}'))));
     }
   }
